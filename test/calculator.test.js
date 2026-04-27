a6304c9e7f2f792ac0a56d2fbe341b5044375c47
royalpinto007 <royalpinto007@gmail.com>
2026-04-27 21:43:28 +0530

    Add isPrime tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { add, subtract, multiply, calculateDiscount, getArea } = require("../src/calculator");
+const { add, subtract, multiply, calculateDiscount, getArea, isPrime } = require("../src/calculator");
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(subtract(5, 2), 3);
@@ -8,5 +8,9 @@ assert.strictEqual(calculateDiscount(100, true), 80);
 assert.strictEqual(calculateDiscount(100, false), 90);
 assert.strictEqual(getArea("square", 4), 16);
 assert.strictEqual(getArea("circle", 5), Math.PI * 25);
+assert.strictEqual(isPrime(2), true);
+assert.strictEqual(isPrime(4), false);
+assert.strictEqual(isPrime(7), true);
+assert.strictEqual(isPrime(9), false);
 
 console.log("All tests passed!");
