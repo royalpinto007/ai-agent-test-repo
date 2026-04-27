6caa398cf92e3c9ad7d382613bf19172b248bb02
royalpinto007 <royalpinto007@gmail.com>
2026-04-27 21:36:56 +0530

    Add getArea tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,10 +1,12 @@
 const assert = require("assert");
-const { add, subtract, multiply, calculateDiscount } = require("../src/calculator");
+const { add, subtract, multiply, calculateDiscount, getArea } = require("../src/calculator");
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(subtract(5, 2), 3);
 assert.strictEqual(multiply(4, 3), 12);
 assert.strictEqual(calculateDiscount(100, true), 80);
 assert.strictEqual(calculateDiscount(100, false), 90);
+assert.strictEqual(getArea("square", 4), 16);
+assert.strictEqual(getArea("circle", 5), Math.PI * 25);
 
 console.log("All tests passed!");
