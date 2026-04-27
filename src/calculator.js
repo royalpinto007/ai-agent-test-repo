a9f6dd52f7aa425e1e48503baeb944967e16251c
royalpinto007 <royalpinto007@gmail.com>
2026-04-27 21:22:36 +0530

    Add calculateDiscount() with swapped discount bug

--- src/calculator.js
+++ src/calculator.js
@@ -10,8 +10,13 @@ function multiply(a, b) {
   return a * b;
 }
 
+function calculateDiscount(price, isPremium) {
+  return isPremium ? price * 0.9 : price * 0.8;
+}
+
 module.exports = {
   add,
   subtract,
-  multiply
+  multiply,
+  calculateDiscount
 };
