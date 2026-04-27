47f04940cc9493319571f529ec143f3c211b94a6
royalpinto007 <royalpinto007@gmail.com>
2026-04-27 21:50:17 +0530

    Add celsiusToFahrenheit tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { add, subtract, multiply, calculateDiscount, getArea, isPrime } = require("../src/calculator");
+const { add, subtract, multiply, calculateDiscount, getArea, isPrime, celsiusToFahrenheit } = require("../src/calculator");
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(subtract(5, 2), 3);
@@ -12,5 +12,7 @@ assert.strictEqual(isPrime(2), true);
 assert.strictEqual(isPrime(4), false);
 assert.strictEqual(isPrime(7), true);
 assert.strictEqual(isPrime(9), false);
+assert.strictEqual(celsiusToFahrenheit(0), 32);
+assert.strictEqual(celsiusToFahrenheit(100), 212);
 
 console.log("All tests passed!");
