724808c3481f164ddebe18e62f04bedcf14e1078
royalpinto007 <royalpinto007@gmail.com>
2026-04-28 10:18:37 +0530

    Add average tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { add, subtract, multiply, calculateDiscount, getArea, isPrime, celsiusToFahrenheit, clamp, power } = require("../src/calculator");
+const { add, subtract, multiply, calculateDiscount, getArea, isPrime, celsiusToFahrenheit, clamp, power, average } = require("../src/calculator");
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(subtract(5, 2), 3);
@@ -20,5 +20,7 @@ assert.strictEqual(clamp(15, 1, 10), 10);
 assert.strictEqual(power(2, 0), 1);
 assert.strictEqual(power(2, 3), 8);
 assert.strictEqual(power(5, 2), 25);
+assert.strictEqual(average([1, 2, 3]), 2);
+assert.strictEqual(average([10, 20, 30]), 20);
 
 console.log("All tests passed!");
